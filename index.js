e39b54cee57f0e7d6e60ff4b097eedbd32dffbf3
Dmytro Matsepa <matsepadmy@cy-tech.fr>
2021-12-15 14:42:06 +0100

    get a chuck norris joke api

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const config = require("./config.json");
 
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const axios = require('axios')
 
 const prefix = '--';
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', function(msg) {
-    
+
     if (msg.author.bot) return;
     if (!msg.content.startsWith(prefix)) return;
   
@@ -22,11 +23,19 @@ client.on('message', function(msg) {
     if (command === "ping") {
         const timeTaken = Date.now() - msg.createdTimestamp;
         msg.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+    
+    }else if (command === "joke") {
+        axios.get('http://api.icndb.com/jokes/random')
+            .then( response => {console.log(response.data),
+                msg.reply(response.data.value.joke)
+            })
+            .catch( error => console.log(error))
     }
+
 });
 
 
 
-
+// login method to start the bot
 client.login(config.CHUCK_TOKEN);
 
