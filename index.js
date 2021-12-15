890bd6747da1f6e0279f343184125c22d1a0c8a8
Dmytro Matsepa <matsepadmy@cy-tech.fr>
2021-12-15 12:20:58 +0100

    prefix added

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 // const { Discord, Client, Intents } = require("discord.js");
 const config = require("./config.json");
 
-
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+const prefix = '--';
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-    if (msg.content === 'ping') {
-      msg.reply('Pong!');
+client.on('message', function(msg) {
+    
+    if (msg.author.bot) return;
+    if (!msg.content.startsWith(prefix)) return;
+  
+    const commandBody = msg.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+  
+    if (command === "ping") {
+        const timeTaken = Date.now() - msg.createdTimestamp;
+        msg.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
     }
 });
+
+
 
 
 client.login(config.CHUCK_TOKEN);
