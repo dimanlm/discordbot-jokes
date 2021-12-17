5306f957fe2f74934384bbd05a897698a6b58886
Dmytro Matsepa <matsepadmy@cy-tech.fr>
2021-12-17 16:02:47 +0100

    Added 'ID' argument to the joke command

--- commands/jokes.js
+++ commands/jokes.js
@@ -8,14 +8,28 @@ module.exports = {
         switch (typeof(args[0])) {
 
             case 'string':
-                axios.get('http://api.icndb.com/jokes/random?limitTo=[' +args[0]+ ']')
-                    .then( response => {
-                        if (response.data.value.joke===undefined){
-                            msg.reply('**' + args[0] + '** category does not exist! \nUse `jokeCategories` command to see the list of categories');
-                        }else{
-                        msg.reply('**' + args[0] + '** joke: ' + response.data.value.joke)}
-                    })
-                    .catch( error => console.log(error));
+                const argValue = parseInt(args[0]);
+                if (Number.isNaN(argValue)){
+                    axios.get('http://api.icndb.com/jokes/random?limitTo=[' +args[0]+ ']')
+                        .then( response => {
+                            if (response.data.value.joke===undefined){
+                                msg.reply('**' + args[0] + '** category does not exist! \nUse `jokeCategories` command to see the list of categories');
+                            }else{
+                                msg.reply('**' + args[0] + '** joke: ' + response.data.value.joke)}
+                        })
+                        .catch( error => console.log(error));
+                }else{
+                    axios.get('http://api.icndb.com/jokes/' + argValue)
+                        .then( response => {
+                            if (response.data.value.joke===undefined){
+                                msg.reply('Joke number **' + args[0] + '** does not exist! \nUse `jokeCount` command to see the how many jokes there are');
+                            }else{
+                                msg.reply('Joke n**' + args[0] + '**: ' + response.data.value.joke)
+                            }
+                        })
+                        .catch( error => console.log(error));
+                }
+
                 break;
 
             default:
